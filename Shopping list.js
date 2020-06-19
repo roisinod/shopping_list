c924c7f49852664f6f1b5d7e0e0c6864b26d8f38
Róisin O'Donnell <55452431+roisinod@users.noreply.github.com>
2020-06-19 12:40:14 +0100

    Add files via upload

---
+++ Shopping list.js
@@ -0,0 +1,17 @@
+/*
+var fname = 'John';
+var lname = prompt('What is his last name?');
+document.write(fname + ' ' + lname);
+
+*/
+function additem()
+{
+var input = document.getElementById("item").value;
+document.write(input);
+}
+if (document.orderForm.mailListBox.checked)
+{
+     addToMailingList();
+     document.write(addToMailingList);
+}
+
